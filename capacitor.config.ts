597e9c8efb1462515b891aecadf494eb7498db31
Sandro Scalco <sandro.scalco@liitu.ch>
2023-02-15 12:56:19 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-club-juno',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
